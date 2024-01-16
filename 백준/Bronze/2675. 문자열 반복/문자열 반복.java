ba0d453220157cc7d8a2f96c2e6ebe115a6abe1c
monghwadang <156163462+monghwadang@users.noreply.github.com>
2024-01-17 08:47:35 +0900

    [Bronze II] Title: 문자열 반복, Time: 228 ms, Memory: 17344 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int i=0; i < T; i++) {
+			
+			int R = sc.nextInt();
+			String S = sc.next();
+			int n = S.length();
+			
+			for(int j=0; j < n; j++) {
+				for(int k=0; k < R; k++) {
+				System.out.print(S.charAt(j));
+				}
+			}
+			
+			System.out.println();
+		}
+		
+	}
+
+
+}
