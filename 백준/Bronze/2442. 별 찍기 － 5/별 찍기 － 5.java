40b797cdf0785baccf3a9f77ec8fe4fc61e6655f
monghwadang <156163462+monghwadang@users.noreply.github.com>
2024-01-21 01:25:33 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 436 ms, Memory: 22444 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		
+		for(int r=0; r<N; r++) {
+			
+			for(int c=0; c<N-1-r; c++) {
+				System.out.print(" ");
+			}
+			for(int c=0; c<2*r+1; c++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+
+		
+		
+	}
+
+}
