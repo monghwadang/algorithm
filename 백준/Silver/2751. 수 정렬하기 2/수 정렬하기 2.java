e3705f2963539d0ebbde34172c7c2777010a76fe
monghwadang <156163462+monghwadang@users.noreply.github.com>
2024-01-30 00:49:09 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2532 ms, Memory: 145684 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Arrays.sort(arr);
+
+        for (int n : arr) {
+            bw.write(n + "\n");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
