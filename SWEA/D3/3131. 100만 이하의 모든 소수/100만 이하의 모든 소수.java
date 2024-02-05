fa446c21b977d7c3a239758881238d396cc1e42b
monghwadang <156163462+monghwadang@users.noreply.github.com>
2024-02-05 14:21:18 +0900

    [D3] Title: 100만 이하의 모든 소수, Time: 468 ms, Memory: 25,756 KB -BaekjoonHub

---
+++ SWEA/D3/3131. 100만 이하의 모든 소수/100만 이하의 모든 소수.java
@@ -0,0 +1,35 @@
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=2; i<=1000000; i++) {
+			
+			if(isPrime(i)) {
+				sb.append(i).append(' ');
+			}
+			
+		}
+		
+		System.out.println(sb);
+
+		
+	}
+
+	public static Boolean isPrime(int num) {
+		
+		if(num < 2) {
+			return false;
+		}
+		
+		for(int i=2; i<=Math.sqrt(num); i++) {
+			if(num % i == 0) {
+				return false;
+			}
+		}
+		
+		return true;
+		
+	}
+}
