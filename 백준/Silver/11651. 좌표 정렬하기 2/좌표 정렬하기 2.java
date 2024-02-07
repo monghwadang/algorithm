074b1bc0b0e29580b735b245dd29c1be7ed5e8e2
monghwadang <156163462+monghwadang@users.noreply.github.com>
2024-02-08 08:55:10 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 692 ms, Memory: 52180 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static class Coordinate {
+		
+		int x;
+		int y;
+		
+		public Coordinate(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Coordinate[] c = new Coordinate[N];
+		
+		for(int i=0; i<N; i++) {
+			
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			c[i] = new Coordinate(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			
+		}
+		
+		Arrays.sort(c, new Comparator<Coordinate>() {
+
+			@Override
+			public int compare(Coordinate o1, Coordinate o2) {
+				if(o1.y == o2.y) {
+					return o1.x - o2.x;
+				}
+				return o1.y - o2.y;
+			}
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(Coordinate coor : c) {
+			sb.append(coor.x).append(' ').append(coor.y).append('\n');
+		}
+		
+		System.out.println(sb);
+		
+		
+	}
+
+}
