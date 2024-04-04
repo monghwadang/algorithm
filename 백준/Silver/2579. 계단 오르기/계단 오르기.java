3ae4e848b03cf503329ff12c53e951a15799eea2
monghwadang <156163462+monghwadang@users.noreply.github.com>
2024-04-04 12:27:50 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -4,11 +4,12 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		
 		int[] stairs = new int[N+1];
 		int[] dp = new int[N+1];
 		
@@ -18,16 +19,18 @@ public class Main {
 		
 		dp[1] = stairs[1];
 		
-		if(N>=2) dp[2] = stairs[1] + stairs[2];
+		if(N>=2) {
+			dp[2] = Math.max(stairs[2], dp[1] + stairs[2]);			
+		}
 		
 		for(int i=3; i<=N; i++) {
-			dp[i] = Math.max(dp[i-2], dp[i-3]+stairs[i-1])+stairs[i];
+			// 한 계단 또는 두 계단
+			// 연속된 세 계단 밟을 수 없음
+			dp[i] = Math.max(dp[i-3]+stairs[i-1]+stairs[i], dp[i-2]+stairs[i]);
 		}
-				
+		
 		System.out.println(dp[N]);
 		
-		
 	}
-	
-	
+
 }
