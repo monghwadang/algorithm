57c2ac286d3680d03ec1e16e5f2332c1d1c50f85
monghwadang <156163462+monghwadang@users.noreply.github.com>
2024-04-04 12:41:54 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -5,40 +5,33 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static int[][] RGB;
-	static boolean[][] visited;
-	static int min = Integer.MAX_VALUE;
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		RGB = new int[N][3];
-		visited = new boolean[N][3];
+		int N = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < N; i++) {
+		int[][] RGB = new int[N+1][3];
+		int[][] dp = new int[N + 1][3];
+
+		for (int r = 1; r <= N; r++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < 3; j++) {
-				RGB[i][j] = Integer.parseInt(st.nextToken());
+			for (int c = 0; c < 3; c++) {
+				RGB[r][c] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		for (int i = 1; i < N; i++) {
-
-			RGB[i][0] += Math.min(RGB[i - 1][1], RGB[i - 1][2]);
-			RGB[i][1] += Math.min(RGB[i - 1][0], RGB[i - 1][2]);
-			RGB[i][2] += Math.min(RGB[i - 1][0], RGB[i - 1][1]);
-
-		}
-
-		int min = Integer.MIN_VALUE;
-		for (int i = 0; i < 3; i++) {
-			min = Math.min(Math.min(RGB[N - 1][0], RGB[N - 1][1]), RGB[N - 1][2]);
+		
+		dp[1][0] = RGB[1][0];
+		dp[1][1] = RGB[1][1];
+		dp[1][2] = RGB[1][2];
+		
+		for(int i=2; i<=N; i++) {			
+			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + RGB[i][0];
+			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + RGB[i][1];
+			dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + RGB[i][2];
 		}
 		
-		System.out.println(min);
-
+		System.out.println(Math.min(Math.min(dp[N][0], dp[N][1]), dp[N][2]));
+		
 	}
 
 }
