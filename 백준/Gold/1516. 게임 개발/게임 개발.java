87f61d5d15e51573afadf3bb4d32b824123da28c
monghwadang <156163462+monghwadang@users.noreply.github.com>
2024-04-14 00:15:59 +0900

    [Gold III] Title: 게임 개발, Time: 272 ms, Memory: 21684 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,89 +1,75 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
- 
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<ArrayList<Integer>> a = new ArrayList<>();
- 
-        for (int i = 0; i <= N; i++) {
-            a.add(new ArrayList<>());
-        }
- 
-        int[] indegree = new int[N + 1]; // 특정 건물을 짓기 전에 먼저 지어야 할 건물의 개수
-        int[] times = new int[N + 1]; // 특정 건물을 짓는 데 걸리는 시간
- 
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
- 
-            times[i] = Integer.parseInt(st.nextToken());
-            while (true) {
-                int num = Integer.parseInt(st.nextToken());
- 
-                if (num == -1) {
-                    break;
-                }
- 
-                a.get(num).add(i);
- 
-                indegree[i]++;
-            }
-        }
- 
-        String ans = topologicalSort(a, indegree, times, N);
- 
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-    
-    // 위상 정렬
-    public static String topologicalSort(ArrayList<ArrayList<Integer>> a, int[] indegree, int[] times, int N) {
-        Queue<Integer> q = new LinkedList<>();
-        StringBuilder sb = new StringBuilder();
-        
-        // 먼저 지어야할 건물이 없는 건물을 큐에 집어 넣음.
-        for (int i = 1; i <= N; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
-            }
-        }
-        
-        // 특정 건물을 짓기 전까지 걸린 시간
-        int[] result = new int[N + 1];
- 
-        while (!q.isEmpty()) {
-            int now = q.poll();
- 
-            for (int next : a.get(now)) {
-                indegree[next]--;
-                
-                // next 건물을 짓기 전까지 걸린 시간 계산.
-                result[next] = Math.max(result[next], result[now] + times[now]);
- 
-                if (indegree[next] == 0) {
-                    q.offer(next);
-                }
-            }
-        }
-        
-        // 특정 건물을 짓는 데 걸린 시간을 출력.
-        for (int i = 1; i <= N; i++) {
-            sb.append((result[i] + times[i]) + "\n");
-        }
- 
-        return sb.toString();
-    }
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine()); // 건물의 수
+		int[] time = new int[N + 1];
+		int[][] adj = new int[N + 1][N + 1]; // 인접행렬
+		int[] degree = new int[N + 1]; // 진입차수 저장
+
+		for (int i = 1; i <= N; i++) {
+
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			time[i] = Integer.parseInt(st.nextToken());
+
+			int n;
+
+			while ((n = Integer.parseInt(st.nextToken())) != -1) {
+
+				adj[n][i] = 1;
+				degree[i]++;
+
+			}
+
+		}
+
+		Queue<Integer> q = new LinkedList<>();
+
+		for (int i = 1; i <= N; i++) {
+			if (degree[i] == 0) {
+				q.offer(i);
+			}
+
+		}
+
+		int[] res = new int[N + 1];
+
+		while (!q.isEmpty()) {
+
+			int curr = q.poll();
+
+			for (int i = 1; i <= N; i++) {
+
+				if (adj[curr][i] == 1) {
+					degree[i]--;
+					
+					// i번째 건물을 짓기 전까지 걸린 시간
+					res[i] = Math.max(res[i], res[curr] + time[curr]);
+
+					if (degree[i] == 0) {
+						q.offer(i);
+					}
+
+				}
+
+			}
+
+		}
+
+		for(int i=1; i<=N; i++) {
+			System.out.println(res[i] + time[i]);
+		}
+
+	}
+
 }
