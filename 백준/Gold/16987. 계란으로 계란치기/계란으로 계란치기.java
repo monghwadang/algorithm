062503d0d3421fe40fef088984ea3b55c0122494
monghwadang <156163462+monghwadang@users.noreply.github.com>
2024-06-17 17:51:37 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 220 ms, Memory: 16880 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -7,62 +7,64 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, cnt;
+	static int N;
 	static int max = Integer.MIN_VALUE;
 	static int[][] egg;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		input();
-		breakEgg(1);
+		breakEgg(1, 0, 0);
 		System.out.println(max);
 
 	}
 
-	private static void breakEgg(int idx) {
-
-
-		if (idx == N+1) { // 제일 오른쪽 계란까지 들었다면
+	// idx번째 계란을 든다.
+	// total : 전 깨뜨린 계란
+	// cnt : 이번 시행으로 깨뜨린 계란
+	private static void breakEgg(int idx, int total, int cnt) {
+		
+		if (idx == N + 1) { // 제일 오른쪽 계란까지 들었을 때
 			
-			cnt = 0;
-
-			for (int i = 1; i < egg.length; i++) {
-				if (egg[i][0] <= 0)
-					cnt++;
-			}
-
-			max = Math.max(cnt, max);
-
+			max = Math.max(total, max);
 			return;
 
 		}
 
 		if (egg[idx][0] <= 0) { // 내구도가 0 이하
-			breakEgg(idx+1);
+			breakEgg(idx + 1, total, 0);
 			return;
 		}
-		
+
 		boolean broken = false;
 		for (int i = 1; i < egg.length; i++) {
 			if (idx != i && egg[i][0] > 0) { // 자기 자신과 이미 깨져 있는 것은 제외
+
+				cnt = 0;
 
 				// 깨뜨리기
 				egg[idx][0] -= egg[i][1];
 				egg[i][0] -= egg[idx][1];
 
+				if (egg[idx][0] <= 0)
+					cnt++;
+				if (egg[i][0] <= 0)
+					cnt++;
+
 				// 다음 계란 잡기
 				broken = true;
-				breakEgg(idx + 1);
+				breakEgg(idx + 1, total + cnt, 0);
 
 				// 원복
 				egg[idx][0] += egg[i][1];
 				egg[i][0] += egg[idx][1];
-				
+
 			}
 		}
-		
+
 		// 잡을 계란이 없었다
-		if(!broken) breakEgg(idx+1);
+		if (!broken)
+			breakEgg(idx + 1, total, 0);
 
 	}
 
