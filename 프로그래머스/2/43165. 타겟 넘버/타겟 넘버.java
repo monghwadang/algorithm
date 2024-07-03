7ce5cb8ff11ea3765eaad8b4b477da74814c26f4
monghwadang <156163462+monghwadang@users.noreply.github.com>
2024-07-03 13:32:21 +0900

    [level 2] Title: 타겟 넘버, Time: 0.35 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,35 +2,34 @@ import java.util.*;
 
 class Solution {
     
-    static int res;
-    static boolean[] visited;
-    
+    static int[] numbers;
+    static int target, res;
+
     public int solution(int[] numbers, int target) {
         
-        int sum = 0; // 숫자 배열의 합
-        for(int i=0; i<numbers.length; i++){
-            sum += numbers[i];
-        }
-        
-        // 부분집합
-        visited = new boolean[numbers.length];
+        this.numbers = numbers;
+        this.target = target;
+
         res = 0;
-        powerset(0, numbers, sum, target);
-        
+        dfs(0,0);
+
         int answer = res;
         return answer;
     }
     
-    public void powerset(int idx, int[] numbers, int sum, int target) {
-        if(idx>=numbers.length){
-            if(sum == target) res++;
+    public void dfs(int idx, int sum){
+        
+        if(idx==numbers.length){
+            if(sum == target){
+                res++;
+            }
             return;
         }
         
-        visited[idx] = true;
-        powerset(idx+1, numbers, sum  - 2 * numbers[idx], target);
+        dfs(idx+1, sum + numbers[idx]);
+        dfs(idx+1, sum - numbers[idx]);
         
-        visited[idx] = false;
-        powerset(idx+1, numbers, sum, target);
     }
+
+
 }
